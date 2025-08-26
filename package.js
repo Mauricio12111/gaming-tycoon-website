989d7bb2714ce1a698e78590d945f5c7f770f97a
gaming-tycoon-website <didimbanza714@gmail.com>
2025-08-26 02:06:26 +0100

    package.js

--- package.js
+++ package.js
@@ -1,14 +1,31 @@
 {
-  "name": "gaming-tycoon-website-1",
+  "name": "mangrat-server",
   "version": "1.0.0",
+  "description": "Un serveur express pour un système de Q&R avec auto-apprentissage et base de données MySQL.",
   "main": "server.js",
+  "type": "module",
   "scripts": {
-    "start": "node server.js"
+    "start": "node server.js",
+    "dev": "nodemon server.js"
   },
+  "keywords": [
+    "express",
+    "mysql",
+    "gemini",
+    "ia",
+    "auto-apprentissage"
+  ],
+  "author": "Mauricio",
+  "license": "ISC",
   "dependencies": {
-    "express": "^4.18.2",
-    "mysql2": "^3.5.2",
+    "@google/generative-ai": "^0.1.3",
     "body-parser": "^1.20.2",
-    "cors": "^2.8.5"
+    "cors": "^2.8.5",
+    "dotenv": "^16.3.1",
+    "express": "^4.18.2",
+    "mysql2": "^3.6.5"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
   }
 }
